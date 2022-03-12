533ea0b9478c1213ddc189078740c97b0e251617
greasemonkey303 <zachsfreed@icloud.com>
2022-03-12 02:54:07 -0500

    start of vid 22

--- ecommerce/frontend/src/store.js
+++ ecommerce/frontend/src/store.js
@@ -1,14 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 
-const initialState = {}
+
 
 const middleware =[thunk]
 
-const store = createStore(reducer,initalState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
